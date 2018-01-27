dfd309c6d3adf347947d7345e3fd28b329576f9f
frkhit <frkhit@gmail.com>
2018-01-28 01:29:00 +0800

    fix bug

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ if not os.path.exists(PROJECT_ROOT):
 FREEZER_DESTINATION = PROJECT_ROOT
 # Since this is a repo page (not a Github user page),
 # we need to set the BASE_URL to the correct url as per GH Pages' standards
-FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
+FREEZER_BASE_URL = "http://localhost"
 FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
 # will be deleted when you run the freezer
 FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
