3c35c35ff872aa837ba835a15a387c1a64671e12
frkhit <frkhit@gmail.com>
2018-01-28 01:10:12 +0800

    clean code

--- freeze.py
+++ freeze.py
@@ -1,4 +1,11 @@
+import os
+import shutil
+
 from project import freezer
+from project.settings import blog_path
 
 if __name__ == '__main__':
     freezer.freeze()
+    
+    if os.path.exists(os.path.join(blog_path, "index.html")):
+        shutil.copy(os.path.join(blog_path, "index.html"), "./index.html")
--- project/views.py
+++ project/views.py
@@ -12,7 +12,7 @@ def home():
     posts = [_page for _page in pages if 'date' in _page.meta]
     # Sort pages by date
     sorted_posts = sorted(posts, reverse=True, key=lambda _page: _page.meta['date'])
-    return render_template('index.html', pages=sorted_posts, git_path=GIT_PATH)
+    return render_template('index.html', pages=sorted_posts, git_path=GIT_PATH, page_title="目录")
 
 
 @app.route('/<path:path>/')
@@ -20,4 +20,4 @@ def page(path):
     # Path is the filename of a page, without the file extension
     # e.g. "first-post"
     _page = pages.get_or_404(path)
-    return render_template('page.html', page=_page, git_path=GIT_PATH)
+    return render_template('page.html', page=_page, git_path=GIT_PATH, page_title=_page.meta["title"])
