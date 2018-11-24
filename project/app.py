d853f12ba4195983f62f19a82165171af0329e56
frkhit <frkhit@gmail.com>
2018-11-25 03:19:38 +0800

    clean code

--- project/app.py
+++ project/app.py
@@ -1,124 +1,148 @@
 # -*- coding:utf-8 -*-
-from __future__ import unicode_literals
 from __future__ import absolute_import
 
 import datetime
 import hashlib
+import json
+import logging
 import operator
 import os
+import pickle
 import re
 import sqlite3
-import threading
+import time
 from itertools import takewhile
 
+import requests
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask_flatpages.page import Page as OldPage, cached_property, yaml
 from flask_frozen import Freezer
 from werkzeug.utils import import_string
 
-
-class SingletonMixin(object):
-    """
-    thread safe singleton base class
-    refer: https://gist.github.com/werediver/4396488
-
-    # Based on tornado.ioloop.IOLoop.instance() approach.
-    # See https://github.com/facebook/tornado
-    """
-    __singleton_lock = threading.Lock()
-    __singleton_instance = None
-    
-    @classmethod
-    def instance(cls):
-        """
-
-        :rtype: SingletonMixin
-        """
-        if not cls.__singleton_instance:
-            with cls.__singleton_lock:
-                if not cls.__singleton_instance:
-                    cls.__singleton_instance = cls()
-        return cls.__singleton_instance
+logger = logging.getLogger(__name__)
 
 
-class HtmlCache(SingletonMixin):
-    def __init__(self):
-        from project.settings import PROJECT_ROOT
-        
-        self.cache_file = os.path.join(PROJECT_ROOT, "./cache.dat")
-    
-    @classmethod
-    def instance(cls):
-        """
+class AbstractCache(object):
+    def __init__(self, cache_file: str):
+        self.cache_file = cache_file
 
-        :rtype: HtmlCache
-        """
-        return super(HtmlCache, cls).instance()
-    
-    def get_content_id_and_html(self, md_id):
-        """
+    def get(self, key: str) -> object:
+        return self._get(key)
 
-        :type md_id: str
-        :rtype: (str, str)
-        """
+    def set(self, key: str, value: object, ttl: int = 1e10):
+        return self._set(key, value, ttl)
+
+    def _set(self, key: str, value: object, ttl: int):
+        raise NotImplemented
+
+    def _get(self, key: str) -> object:
+        raise NotImplemented
+
+    @staticmethod
+    def _get_exp_time(ttl: int) -> int:
+        return int(time.time() + ttl)
+
+    @staticmethod
+    def _ttl_is_timeout(exp_time: float) -> bool:
+        return time.time() > exp_time
+
+
+class SqliteCache(AbstractCache):
+    def __init__(self, cache_file: str):
+        super(SqliteCache, self).__init__(cache_file)
+
+    def _get(self, key: str) -> object:
         conn = self._get_connect()
-        
         try:
-            sql = "SELECT CID, VALUE from CACHE where MID = ?"
-            cursor = conn.cursor().execute(sql, (md_id,))
-            for row in cursor:
-                return row[0], row[1]
+            value, exp_time = self._get_uid_value(conn, key)
+            if exp_time is not None and not self._ttl_is_timeout(exp_time):
+                return value
+
         except Exception as e:
-            print(e)
+            logger.error(e)
         finally:
             conn.close()
-        
-        return None, None
-    
-    def set_content_id_and_html(self, md_id, content_id, html_value):
-        conn = self._get_connect()
-        
+
+        return None
+
+    @staticmethod
+    def _get_uid_value(conn, key):
         try:
-            old_content_id, old_html = self.get_content_id_and_html(md_id=md_id)
-            if old_content_id is None and old_html is None:
-                sql = "INSERT INTO CACHE(ID, VALUE, CID, MID) VALUES (NULL, ?, ?, ?)"
+            sql = "SELECT VALUE, EXPTIME from CACHE where UID = ?"
+            cursor = conn.cursor().execute(sql, (key,))
+            for row in cursor:
+                return pickle.loads(row[0]), row[1]
+        except Exception as e:
+            logger.error(e)
+
+        return None, None
+
+    def _set(self, key: str, value: object, ttl: int):
+        conn = self._get_connect()
+        try:
+            old_value, exp_time = self._get_uid_value(conn, key)
+            if old_value is None and exp_time is None:
+                sql = "INSERT INTO CACHE(ID, VALUE, EXPTIME, UID) VALUES (NULL, ?, ?, ?)"
             else:
-                sql = "UPDATE CACHE SET VALUE = ?, CID = ? where MID = ?"
-            
-            conn.cursor().execute(sql, (html_value, content_id, md_id))
+                sql = "UPDATE CACHE SET VALUE = ?, EXPTIME = ? where UID = ?"
+
+            conn.cursor().execute(sql, (pickle.dumps(value), self._get_exp_time(ttl), key))
             conn.commit()
         except Exception as e:
-            print(e)
+            logger.error(e)
             return None
         finally:
             conn.close()
-    
+
     def _get_connect(self):
         if not os.path.exists(self.cache_file):
             conn = sqlite3.connect(self.cache_file)
             c = conn.cursor()
             c.execute('''CREATE TABLE CACHE
                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                   MID CHAR(32) UNIQUE NOT NULL,
-                   CID CHAR(32) NOT NULL,
+                   UID CHAR(32) UNIQUE NOT NULL,
+                   EXPTIME INT NOT NULL ,
                    VALUE TEXT);''')
             conn.commit()
             conn.close()
-        
+
         return sqlite3.connect(self.cache_file)
 
 
 def get_md5(string):
+    """
+    use in python3.6:
+    get_md5("x".encode("utf-8"))
+    """
     hash_md5 = hashlib.md5(string)
     return hash_md5.hexdigest()
 
 
-def md2html_by_github(content):
+class HtmlCache(object):
+    from project.settings import PROJECT_ROOT
+    cache = SqliteCache(os.path.join(PROJECT_ROOT, "./cache.dat"))
+
+    @classmethod
+    def get_content_id_and_html(cls, md_id):
+        """
+
+        :type md_id: str
+        :rtype: (str, str)
+        """
+        result = cls.cache.get(md_id)
+        if result is None:
+            return None, None
+        return result[0], result[1]
+
+    @classmethod
+    def set_content_id_and_html(cls, md_id, content_id, html_value):
+        cls.cache.set(key=md_id, value=(content_id, html_value))
+
+
+def md2html_by_github(content: str) -> str:
     """ use github api """
-    print("requesting github...")
-    import requests
-    import json
+    logging.info("requesting github...")
     url = "https://api.github.com/markdown"
     return requests.post(url, data=json.dumps({"text": content, "mode": "markdown"})).text
 
@@ -132,30 +156,30 @@ class Page(OldPage):
         :type path: str
         """
         super(Page, self).__init__(path, meta, body, html_renderer)
-        
+
         # date re
         self.date_re = re.compile(r"[\d]{4}/[\d]{2}/[\d]{2}(?=/)")
-        
+
         # 执行meta方法
         if self.meta:
             pass
-    
+
     @cached_property
     def html(self):
         """The content of the page, rendered as HTML by the configured
         renderer.
         """
         file_name_id = get_md5(self.path.encode("utf-8"))
-        
-        old_content_id, content_html = HtmlCache.instance().get_content_id_and_html(file_name_id)
+
+        old_content_id, content_html = HtmlCache.get_content_id_and_html(file_name_id)
         current_content_id = get_md5(self.body.encode("utf-8"))
-        
+
         if content_html is None or old_content_id != current_content_id:
             content_html = md2html_by_github(self.body)
-            HtmlCache.instance().set_content_id_and_html(file_name_id, current_content_id, content_html)
-        
+            HtmlCache.set_content_id_and_html(file_name_id, current_content_id, content_html)
+
         return content_html
-    
+
     @cached_property
     def meta(self):
         """A dict of metadata parsed as YAML from the header of the file.
@@ -168,30 +192,30 @@ class Page(OldPage):
             if self._meta:
                 # 还原body
                 self.body = "\n".join((self._meta, self.body))
-            
+
             meta = {}
-        
+
         if not isinstance(meta, dict):
             raise ValueError("Excpected a dict in metadata for '{0}', got {1}".
                              format(self.path, type(meta).__name__))
-        
+
         # title
         if "title" not in meta:
             _title = None
             if self._meta:
                 _title = self._meta.replace("#", "").strip()
-            
+
             if _title is None:
                 _title = os.path.basename(self.path)
-            
+
             meta["title"] = _title or "日志"
-        
+
         # date
         if "date" not in meta:
             meta["date"] = self._get_date_from_path(self.path) or datetime.date.today()
-        
+
         return meta
-    
+
     def _get_date_from_path(self, path):
         """
             文件命名: /2018/01/31/xxx.md
@@ -203,34 +227,34 @@ class Page(OldPage):
         return None
 
 
-def _parse(self, content, path):
+def flat_pages_parse(self, content, path):
     """Parse a flatpage file, i.e. read and parse its meta data and body.
 
     :return: initialized :class:`Page` instance.
     """
     lines = iter(content.split('\n'))
-    
+
     # Read lines until an empty line is encountered.
     meta = '\n'.join(takewhile(operator.methodcaller('strip'), lines))
     # The rest is the content. `lines` is an iterator so it continues
     # where `itertools.takewhile` left it.
     content = '\n'.join(lines)
-    
+
     # Now we ready to get HTML renderer function
     html_renderer = self.config('html_renderer')
-    
+
     # If function is not callable yet, import it
     if not callable(html_renderer):
         html_renderer = import_string(html_renderer)
-    
+
     # Make able to pass custom arguments to renderer function
     html_renderer = self._smart_html_renderer(html_renderer)
-    
+
     # Initialize and return Page instance
     return Page(path, meta, content, html_renderer)
 
 
-FlatPages._parse = _parse
+FlatPages._parse = flat_pages_parse
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
--- project/settings.py
+++ project/settings.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
-from __future__ import unicode_literals
 from __future__ import absolute_import
 
+import logging
 import os
+import sys
 
 REPO_NAME = "frkhit.github.io"  # Used for FREEZER_BASE_URL
 GIT_PATH = "https://github.com/frkhit/frkhit.github.io"  # Used for FREEZER_BASE_URL
@@ -18,6 +19,23 @@ def parent_dir(path):
     return os.path.abspath(os.path.join(path, os.pardir))
 
 
+def init_logger(log_file=None, logger_lever=logging.INFO):
+    _default_formatter = logging.Formatter("%(asctime)s %(filename)s - %(name)s - %(levelname)s - %(message)s", None)
+
+    root_logger = logging.getLogger()
+    _console_handler = logging.StreamHandler(sys.stdout)
+    _console_handler.setFormatter(_default_formatter)
+    root_logger.addHandler(_console_handler)
+
+    # file handler
+    if log_file is not None:
+        _file_handler = logging.FileHandler(log_file)
+        _file_handler.setFormatter(_default_formatter)
+        root_logger.addHandler(_file_handler)
+
+    root_logger.setLevel(logger_lever)
+
+
 PROJECT_ROOT = parent_dir(APP_DIR)
 if not os.path.exists(PROJECT_ROOT):
     os.makedirs(PROJECT_ROOT)
@@ -31,3 +49,4 @@ FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
 FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
 FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
 FLATPAGES_EXTENSION = '.md'
+init_logger()
